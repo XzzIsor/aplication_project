9849b54ceafbe7faf5f116f4476f12d828006f90
Esteban R <70593544+XzzIsor@users.noreply.github.com>
2021-01-16 03:17:38 -0500

    Update1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_aplication/screens/home_page.dart';
 
-void main() {
-  runApp(HomePage());
+void main() => runApp(App());
+
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
 }
